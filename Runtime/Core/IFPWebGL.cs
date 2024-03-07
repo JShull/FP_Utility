276546edb84eb1c0e7550b455b014197fc044513
John Shull <the.john.shull@gmail.com>
2024-03-07 13:36:24 -0500

    Update IFPWebGL.cs

--- Runtime/Core/IFPWebGL.cs
+++ Runtime/Core/IFPWebGL.cs
@@ -11,4 +11,28 @@ namespace FuzzPhyte.Utility
     {
         public void OnFullScreenChange(string message);
     }
+    // Unity example of use
+    /*
+     *  // Full screen listener example on the js side
+      
+    function unityFullScreenListener() {
+        document.addEventListener("fullscreenchange", function() {
+            var isInFullScreen = document.fullscreenElement != null;
+            unityInstance.SendMessage('GenericController', 'OnFullScreenChange', isInFullScreen.toString());
+        });
+    }
+     *
+     //UNITY side
+     * public void OnFullScreenChange(string isInFullScreen)
+        {
+            bool isFullScreen = bool.Parse(isInFullScreen);
+            Debug.Log("Full-screen mode: " + isFullScreen);
+            if (isFullScreen)
+            {
+                //open the menu
+                MenuOpenUI();
+            }
+            // Add any additional logic here for when the full-screen state changes
+        }
+     */
 }
