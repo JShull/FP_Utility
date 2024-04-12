c047765fe5e1f4db8436f198eba30ae178fefd30
John Shull <the.john.shull@gmail.com>
2024-04-12 02:55:20 -0400

    Update FP_Utility_Editor.cs
    Hot fix on folder names

--- Editor/FP_Utility_Editor.cs
+++ Editor/FP_Utility_Editor.cs
@@ -180,7 +180,9 @@ namespace FuzzPhyte.Utility.Editor
         /// <returns>first string is the local path assuming you're at Assets, the second string is the full path</returns>
         public static (string,string) CreatePackageSampleFolder(string productName,string version)
         {
-            var localSamplePath = "Samples//" + productName + "//";
+            var productSlashName = productName+"/";
+            var localSamplePath = Path.Combine("Samples", productSlashName);
+            
             var potentialFolder = Path.Combine(Application.dataPath, localSamplePath);
             if (!File.Exists(potentialFolder))
             {
@@ -199,7 +201,9 @@ namespace FuzzPhyte.Utility.Editor
             {
                 Directory.CreateDirectory(fullDirectory);
             }
-            var assetLocalPath = localSamplePath + version+ "//" + sampleWithinVersion;
+            var localSampleVersion = localSamplePath+version;
+            var assetLocalPath = Path.Combine(localSampleVersion,sampleWithinVersion);
+            //var assetLocalPath = localSamplePath + version+ "//" + sampleWithinVersion;
             return (assetLocalPath, fullDirectory);
         }
 
