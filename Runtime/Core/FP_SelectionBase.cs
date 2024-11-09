ff214f907d1fb97ffd287c13bfa88002ee38cb8e
John Shull <the.john.shull@gmail.com>
2024-11-09 07:51:17 -0500

    Selection Base Root

---
+++ Runtime/Core/FP_SelectionBase.cs
@@ -0,0 +1,11 @@
+namespace FuzzPhyte.Utility
+{
+    using FuzzPhyte.Utility.Meta;
+    using UnityEngine;
+
+    [SelectionBase]
+    public class FP_SelectionBase : MonoBehaviour
+    {
+        public FP_Tag MainFPTag;
+    }
+}
