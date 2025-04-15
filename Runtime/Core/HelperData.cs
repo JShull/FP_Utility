5a072c35435e4456ede1920960da14b1871a1262
John Shull <the.john.shull@gmail.com>
2025-04-15 14:20:51 -0400

    Update HelperData.cs

--- Runtime/Core/HelperData.cs
+++ Runtime/Core/HelperData.cs
@@ -1,13 +1,13 @@
 namespace FuzzPhyte.Utility
 {
     using System;
-
+    using System.Collections.Generic;
     public class HelperData : IComparable<HelperData>
     {
-        public HelperCategory Category { get; set; }
-        public HelperAction HelperAction { get; set; }
+        public List<HelperCategory> Category { get; set; }
+        public List<HelperAction> HelperAction { get; set; }
         public float ActivationTime { get; set; }  // Time when the helper should activate
-        public Action onActivate { get; set; }      // Action to execute when activated
+        public List<Action> onActivate { get; set; }      // Action to execute when activated
         public int CompareTo(HelperData other)
         {
             return ActivationTime.CompareTo(other.ActivationTime);
